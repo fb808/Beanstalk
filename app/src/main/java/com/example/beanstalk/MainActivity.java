69a4b9c2ede7f08d44714f23a1e7e001ded47040
wlals <s2019w26@e-mirim.hs.kr>
2020-11-06 00:32:02 +0900

    first commit

---
+++ app/src/main/java/com/example/beanstalk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.beanstalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); //로그인창부터 시작
+    }
+}
