f69dd6530942d6c4f0c9999be9c6e86313ec7739
wlals <s2019w26@e-mirim.hs.kr>
2020-11-26 14:31:53 +0900

    메뉴추가

--- app/src/main/java/com/example/beanstalk/MainActivity.java
+++ app/src/main/java/com/example/beanstalk/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.beanstalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); //로그인창부터 시작
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_layout, fragment1).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.tab1:{
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout,fragment1).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    case R.id.tab2:{
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout,fragment2).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    case R.id.tab3:{
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout,fragment3).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    default:return false;
+                }
+            }
+        });
     }
 }
