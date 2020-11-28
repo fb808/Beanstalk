8cc94ee76c2cf7ca89b248d2e36d1098882fd400
wlals <s2019w26@e-mirim.hs.kr>
2020-11-28 23:02:06 +0900

    김지연꺼 받아서 논리오류수정

---
+++ app/src/main/java/com/example/beanstalk/MenuActivity.java
@@ -0,0 +1,59 @@
+package com.example.beanstalk;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        BottomNavigationView bottomNavigationView;
+        final Fragment1 fragment1;
+        final Fragment2 fragment2;
+        final Fragment3 fragment3;
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_layout, fragment1).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.tab1: {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout, fragment1).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    case R.id.tab2: {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout, fragment2).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    case R.id.tab3: {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_layout, fragment3).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+}
