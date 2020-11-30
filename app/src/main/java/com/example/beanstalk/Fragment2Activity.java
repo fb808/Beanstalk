2c20c658b2b39121a432dbc63a51a2e74c6c7b5d
wlals <s2019w26@e-mirim.hs.kr>
2020-11-30 14:05:00 +0900

    메인화면 추가

--- app/src/main/java/com/example/beanstalk/Fragment2Activity.java
+++ app/src/main/java/com/example/beanstalk/Fragment2Activity.java
@@ -7,9 +7,12 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Fragment2Activity extends AppCompatActivity {
 
     // 컴퓨터 자격증 정보
+    ArrayList<CertificateData> CerDateList;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,5 +56,29 @@ public class Fragment2Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void InitializeCerData() {
+        CerDateList = new ArrayList<CertificateData>();
+
+        CerDateList.add(new CertificateData("3D프린터개발산업기사", "한국산업인력공단", 19400, 81000));
+        CerDateList.add(new CertificateData("3D프린터운용기능사", "한국산업인력공단", 14500, 27000));
+        CerDateList.add(new CertificateData("로봇소프트웨어개발기사", "한국산업인력공단", 19400, 30300));
+        CerDateList.add(new CertificateData("로봇하드웨어개발기사", "한국산업인력공단", 19400, 54300));
+        CerDateList.add(new CertificateData("멀티미디어콘텐츠제작전문가", "한국산업인력공단", 19400, 26300));
+        CerDateList.add(new CertificateData("정보관리기술사", "한국산업인력공단", 67800, 87100));
+        CerDateList.add(new CertificateData("정보처리기능사", "한국산업인력공단", 14500, 17200));
+        CerDateList.add(new CertificateData("정보처리기사", "한국산업인력공단", 19400, 22600));
+        CerDateList.add(new CertificateData("정보처리산업기사", "한국산업인력공단", 19400, 20800));
+        CerDateList.add(new CertificateData("컴퓨터시스템응용기술사", "한국산업인력공단", 67800, 87100));
+        CerDateList.add(new CertificateData("워드프로세서", "대한상공회의소", 15700, 17800));
+        CerDateList.add(new CertificateData("컴퓨터활용능력1급", "대한상공회의소", 17800, 21000));
+        CerDateList.add(new CertificateData("컴퓨터활용능력2급", "대한상공회의소", 19400, 20800));
+        CerDateList.add(new CertificateData("빅데이터분석기사", "한국데이터산업진흥원", 80000, 70000));
+        CerDateList.add(new CertificateData("정보보안기사", "한국인터넷진흥원", 18800, 21900));
+        CerDateList.add(new CertificateData("정보보안산업기사", "한국인터넷진흥원", 18800, 20200));
+        CerDateList.add(new CertificateData("게임그래픽전문가", "한국콘텐츠진흥원", 18000, 23900));
+        CerDateList.add(new CertificateData("게임기획전문가", "한국콘텐츠진흥원", 18000, 23500));
+        CerDateList.add(new CertificateData("게임프로그래밍전문가", "한국콘텐츠진흥원", 18000, 22700));
     }
 }
--- app/src/main/java/com/example/beanstalk/MenuActivity.java
+++ app/src/main/java/com/example/beanstalk/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.example.beanstalk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +16,80 @@ public class MenuActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment1);
+        setContentView(R.layout.activity_main);
 
+        Button btn_1 = (Button) findViewById(R.id.btn_menu1);
+        Button btn_2 = (Button) findViewById(R.id.btn_menu2);
+        Button btn_3 = (Button) findViewById(R.id.btn_menu3);
+        Button btn_4 = (Button) findViewById(R.id.btn_menu4);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment4Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView menu1 = (ImageView) findViewById(R.id.menu1);
+        ImageView menu2 = (ImageView) findViewById(R.id.menu2);
+        ImageView menu3 = (ImageView) findViewById(R.id.menu3);
+        ImageView menu4 = (ImageView) findViewById(R.id.menu4);
+
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        menu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        menu4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Fragment4Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
